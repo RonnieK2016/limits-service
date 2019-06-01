9583f386674599e9c2a9b5b1abdf081af5e6a838
SergeA <ronnieo@gmail.com>
2019-06-01 14:11:54 -0400

    Added FileBasedLimitsConfiguration for LimitsServiceConfigurationController

---
+++ src/main/java/com/udemy/microservices/limitsservice/config/FileBasedLimitsConfiguration.java
@@ -0,0 +1,16 @@
+package com.udemy.microservices.limitsservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+@Getter
+@Setter
+public class FileBasedLimitsConfiguration {
+
+    private int minimum;
+    private int maximum;
+}
--- src/main/java/com/udemy/microservices/limitsservice/controllers/LimitsServiceConfigurationController.java
+++ src/main/java/com/udemy/microservices/limitsservice/controllers/LimitsServiceConfigurationController.java
@@ -1,5 +1,6 @@
 package com.udemy.microservices.limitsservice.controllers;
 
+import com.udemy.microservices.limitsservice.config.FileBasedLimitsConfiguration;
 import com.udemy.microservices.limitsservice.config.LimitsConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitsServiceConfigurationController {
 
+    private FileBasedLimitsConfiguration configuration;
+
+    public LimitsServiceConfigurationController(FileBasedLimitsConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public LimitsConfiguration getLimitsFromConfiguration() {
-        return new LimitsConfiguration(1, 1000);
+        return new LimitsConfiguration(configuration.getMinimum(), configuration.getMaximum());
     }
 }
