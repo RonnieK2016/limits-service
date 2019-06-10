f1c3aadd482baaf1ef7b8ecb7a38f82438cdc543
SergeA <ronnieo@gmail.com>
2019-06-09 20:54:53 -0400

    Added Hystrix example method

--- src/main/java/com/udemy/microservices/limitsservice/controllers/LimitsServiceConfigurationController.java
+++ src/main/java/com/udemy/microservices/limitsservice/controllers/LimitsServiceConfigurationController.java
@@ -1,5 +1,6 @@
 package com.udemy.microservices.limitsservice.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.udemy.microservices.limitsservice.config.FileBasedLimitsConfiguration;
 import com.udemy.microservices.limitsservice.config.LimitsConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +18,15 @@ public class LimitsServiceConfigurationController {
     @GetMapping("/limits")
     public LimitsConfiguration getLimitsFromConfiguration() {
         return new LimitsConfiguration(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "limitsFallbackMethod")
+    public LimitsConfiguration faultToleranceException() {
+        throw new RuntimeException("Not Implemented");
+    }
+
+    public LimitsConfiguration limitsFallbackMethod() {
+        return new LimitsConfiguration(100, 200);
     }
 }
